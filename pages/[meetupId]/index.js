7f862951d8abf26773df4c2a89e756a8343bdc41
tiendat184.it <dattocngan@gmail.com>
2022-06-14 10:43:53 +0700

    fix fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
     client.close().then();
 
     return {
-        fallback: false,
+        fallback: 'blocking',
         paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() }}))
     }
 }
